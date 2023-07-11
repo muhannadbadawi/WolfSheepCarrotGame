5935f24b7ba3fed11e11e454fce28b9f60a5eea0
muhannadbadawi <88613807+muhannadbadawi@users.noreply.github.com>
2023-07-11 11:30:04 +0300

    a

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.picWolf = new System.Windows.Forms.PictureBox();
+            this.picGoat = new System.Windows.Forms.PictureBox();
+            this.picCarrot = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.picShip = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.ReastButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picWolf)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picGoat)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCarrot)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picShip)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // picWolf
+            // 
+            this.picWolf.BackColor = System.Drawing.Color.Transparent;
+            this.picWolf.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.picWolf.Image = ((System.Drawing.Image)(resources.GetObject("picWolf.Image")));
+            this.picWolf.Location = new System.Drawing.Point(877, 197);
+            this.picWolf.Name = "picWolf";
+            this.picWolf.Size = new System.Drawing.Size(104, 110);
+            this.picWolf.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picWolf.TabIndex = 9;
+            this.picWolf.TabStop = false;
+            this.picWolf.Click += new System.EventHandler(this.picWolf_Click);
+            // 
+            // picGoat
+            // 
+            this.picGoat.BackColor = System.Drawing.Color.Transparent;
+            this.picGoat.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.picGoat.Image = ((System.Drawing.Image)(resources.GetObject("picGoat.Image")));
+            this.picGoat.Location = new System.Drawing.Point(976, 197);
+            this.picGoat.Name = "picGoat";
+            this.picGoat.Size = new System.Drawing.Size(109, 110);
+            this.picGoat.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picGoat.TabIndex = 8;
+            this.picGoat.TabStop = false;
+            this.picGoat.Click += new System.EventHandler(this.picGoat_Click);
+            // 
+            // picCarrot
+            // 
+            this.picCarrot.BackColor = System.Drawing.Color.Transparent;
+            this.picCarrot.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.picCarrot.Image = ((System.Drawing.Image)(resources.GetObject("picCarrot.Image")));
+            this.picCarrot.Location = new System.Drawing.Point(792, 197);
+            this.picCarrot.Name = "picCarrot";
+            this.picCarrot.Size = new System.Drawing.Size(89, 110);
+            this.picCarrot.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picCarrot.TabIndex = 7;
+            this.picCarrot.TabStop = false;
+            this.picCarrot.Click += new System.EventHandler(this.picCarrot_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(792, 299);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(309, 60);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 11;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 299);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(309, 60);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // picShip
+            // 
+            this.picShip.BackColor = System.Drawing.Color.Transparent;
+            this.picShip.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.picShip.Image = ((System.Drawing.Image)(resources.GetObject("picShip.Image")));
+            this.picShip.Location = new System.Drawing.Point(508, 289);
+            this.picShip.Name = "picShip";
+            this.picShip.Size = new System.Drawing.Size(278, 70);
+            this.picShip.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picShip.TabIndex = 13;
+            this.picShip.TabStop = false;
+            this.picShip.Click += new System.EventHandler(this.picShip_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.ReastButton);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1085, 352);
+            this.panel1.TabIndex = 14;
+            // 
+            // ReastButton
+            // 
+            this.ReastButton.Location = new System.Drawing.Point(447, 236);
+            this.ReastButton.Name = "ReastButton";
+            this.ReastButton.Size = new System.Drawing.Size(84, 33);
+            this.ReastButton.TabIndex = 1;
+            this.ReastButton.Text = "Reast";
+            this.ReastButton.UseVisualStyleBackColor = true;
+            this.ReastButton.Click += new System.EventHandler(this.ReastButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Sans Serif Collection", 19.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(319, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(415, 98);
+            this.label1.TabIndex = 0;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1085, 352);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.picShip);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.picWolf);
+            this.Controls.Add(this.picGoat);
+            this.Controls.Add(this.picCarrot);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1103, 399);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(1103, 399);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picWolf)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picGoat)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCarrot)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picShip)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picWolf;
+        private System.Windows.Forms.PictureBox picGoat;
+        private System.Windows.Forms.PictureBox picCarrot;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox picShip;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ReastButton;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            panel1.Hide();
+            picShip.Location = new Point(367, 235);
+            picGoat.Location = new Point(732, 157);
+            picWolf.Location = new Point(650, 165);
+            picCarrot.Location = new Point(594, 175);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show(picShip.Location.ToString());
+            //MessageBox.Show(picGoat.Location.ToString());
+            //MessageBox.Show(picWolf.Location.ToString());
+            //MessageBox.Show(picCarrot.Location.ToString());
+        }
+        private void picGoat_Click(object sender, EventArgs e)
+        {
+            { //move
+                if (picGoat.Location == new Point(732, 157) && picShip.Location == new Point(367, 235))
+                {
+                    picGoat.Location = new Point(400, 157);
+                }
+                else if (picGoat.Location == new Point(400, 157) && picShip.Location == new Point(367, 235))
+                {
+                    picGoat.Location = new Point(732, 157);
+                }
+                else if (picGoat.Location == new Point(280, 157) && picShip.Location == new Point(236, 235))
+                {
+                    picGoat.Location = new Point(140, 157);
+                }
+                else if (picGoat.Location == new Point(140, 157) && picShip.Location == new Point(236, 235))
+                {
+                    picGoat.Location = new Point(280, 157);
+                }
+                if (picGoat.Location == new Point(400, 157) && picWolf.Location == new Point(400, 165))
+                {
+                    picWolf.Location = new Point(650, 165);
+                }
+                else if (picGoat.Location == new Point(400, 157) && picCarrot.Location == new Point(400, 175))
+                {
+                    picCarrot.Location = new Point(594, 175);
+                }
+                if (picShip.Location == new Point(236, 235) && picGoat.Location == new Point(280, 157) && picWolf.Location == new Point(280, 165))
+                {
+                    picWolf.Location = new Point(64, 165);
+                }
+                else if (picShip.Location == new Point(236, 235) && picGoat.Location == new Point(280, 157) && picCarrot.Location == new Point(280, 175))
+                {
+                    picCarrot.Location = new Point(-1, 175);
+                }
+               
+            }
+            if (picGoat.Location == new Point(140, 157) && picWolf.Location == new Point(64, 165) && picCarrot.Location == new Point(-1, 175))
+            {
+                panel1.Show();
+                label1.ForeColor = Color.Green;
+                label1.Text = "Win";
+            }
+        }
+
+
+        private void picWolf_Click(object sender, EventArgs e)
+        {
+            { //move
+                if (picWolf.Location == new Point(650, 165) && picShip.Location == new Point(367, 235))
+                {
+                    picWolf.Location = new Point(400, 165);
+                }
+                else if (picWolf.Location == new Point(400, 165) && picShip.Location == new Point(367, 235))
+                {
+                    picWolf.Location = new Point(650, 165);
+                }
+                else if (picWolf.Location == new Point(280, 165) && picShip.Location == new Point(236, 235))
+                {
+                    picWolf.Location = new Point(64, 165);
+                }
+                else if (picWolf.Location == new Point(64, 165) && picShip.Location == new Point(236, 235))
+                {
+                    picWolf.Location = new Point(280, 165);
+                }
+                if (picGoat.Location == new Point(400, 157) && picWolf.Location == new Point(400, 165))
+                {
+                    picGoat.Location = new Point(732, 157);
+                }
+                else if (picWolf.Location == new Point(400, 165) && picCarrot.Location == new Point(400, 175))
+                {
+                    picCarrot.Location = new Point(594, 175);
+                }
+                if (picShip.Location == new Point(236, 235) && picWolf.Location == new Point(280, 165) && picGoat.Location == new Point(280, 157))
+                {
+                    picGoat.Location = new Point(140, 157);
+                }
+                else if (picShip.Location == new Point(236, 235) && picWolf.Location == new Point(280, 165) && picCarrot.Location == new Point(280, 175))
+                {
+                    picCarrot.Location = new Point(-1, 175);
+                }
+            }
+            if (picGoat.Location == new Point(367, 235) && picCarrot.Location == new Point(594, 175) && picWolf.Location == new Point(64, 165))
+            {
+                panel1.Show();
+                label1.ForeColor = Color.Red;
+                label1.Text = "Losser";
+            }
+
+        } 
+
+        private void picCarrot_Click(object sender, EventArgs e)
+        {
+            {//move
+                if (picCarrot.Location == new Point(594, 175) && picShip.Location == new Point(367, 235))
+                {
+                    picCarrot.Location = new Point(400, 175);
+                }
+                else if (picCarrot.Location == new Point(400, 175) && picShip.Location == new Point(367, 235))
+                {
+                    picCarrot.Location = new Point(594, 175);
+                }
+                else if (picCarrot.Location == new Point(280, 175) && picShip.Location == new Point(236, 235))
+                {
+                    picCarrot.Location = new Point(-1, 175);
+                }
+                else if (picCarrot.Location == new Point(-1, 175) && picShip.Location == new Point(236, 235))
+                {
+                    picCarrot.Location = new Point(280, 175);
+                }
+                if (picCarrot.Location == new Point(400, 175) && picGoat.Location == new Point(400, 157))
+                {
+                    picGoat.Location = new Point(732, 157);
+                }
+                else if (picCarrot.Location == new Point(400, 175) && picWolf.Location == new Point(400, 165))
+                {
+                    picWolf.Location = new Point(650, 165);
+                }
+                if (picShip.Location == new Point(236, 235) && picCarrot.Location == new Point(280, 175) && picGoat.Location == new Point(280, 157))
+                {
+                    picGoat.Location = new Point(140, 157);
+                }
+                else if (picShip.Location == new Point(236, 235) && picCarrot.Location == new Point(280, 175) && picWolf.Location == new Point(280, 165))
+                {
+                    picWolf.Location = new Point(64, 165);
+                }
+            }
+               if (picWolf.Location == new Point(650, 165) && picGoat.Location == new Point(732, 157) && picCarrot.Location == new Point(-1, 175))
+            {
+                panel1.Show();
+                label1.ForeColor = Color.Red;
+                label1.Text = "Losser";
+            }
+        }
+
+
+        private void picShip_Click(object sender, EventArgs e)
+        {
+            {//move
+                if (picShip.Location == new Point(367, 235))
+                {
+                    picShip.Location = new Point(236, 235);
+                }
+                else if (picShip.Location == new Point(236, 235))
+                {
+                    picShip.Location = new Point(367, 235);
+                }
+                if (picGoat.Location == new Point(400, 157))
+                {
+                    picGoat.Location = new Point(280, 157);
+                }
+                else if (picGoat.Location == new Point(280, 157))
+                {
+                    picGoat.Location = new Point(400, 157);
+                }
+                else if (picWolf.Location == new Point(400, 165))
+                {
+                    picWolf.Location = new Point(280, 165);
+                }
+                else if (picWolf.Location == new Point(280, 165))
+                {
+                    picWolf.Location = new Point(400, 165);
+                }
+                else if (picCarrot.Location == new Point(400, 175))
+                {
+                    picCarrot.Location = new Point(280, 175);
+                }
+                else if (picCarrot.Location == new Point(280, 175))
+                {
+                    picCarrot.Location = new Point(400, 175);
+                }
+            }
+        
+         
+            if (picGoat.Location == new Point(140, 157) && picCarrot.Location == new Point(-1, 175)&& picWolf.Location == new Point(650, 165))
+            {
+                panel1.Show();
+                label1.ForeColor = Color.Red;
+                label1.Text = "Losser";
+            }
+            else if (picWolf.Location == new Point(64, 165) && picGoat.Location == new Point(140, 157) && picCarrot.Location == new Point(594, 175))
+            {
+                panel1.Show();
+                label1.ForeColor = Color.Red;
+                label1.Text = "Losser";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void ReastButton_Click(object sender, EventArgs e)
+        {
+            panel1.Hide();
+            picShip.Location = new Point(367, 235);
+            picGoat.Location = new Point(732, 157);
+            picWolf.Location = new Point(650, 165);
+            picCarrot.Location = new Point(594, 175);
+        }
+    }
+}
+
+
+
+// goat 140 wolf 64 carrot -1 
